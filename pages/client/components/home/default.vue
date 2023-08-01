af0b2055ba33e6ec9301b6a41fa42a89ef2c3d84
woaixing <103154274+woaixing@users.noreply.github.com>
2023-08-01 19:26:05 +0800

    Add files via upload

---
+++ pages/client/components/home/default.vue
@@ -0,0 +1,140 @@
+<template>
+	<view>
+		<view class="pd16_15 mt15" style="margin-bottom: 80upx;">	
+			<view class="flex alcenter space" style="margin-bottom: 10upx;">
+				<view class="flex alcenter">
+					<image style="width: 40rpx;height: 40rpx;" :src="statics.zhuico[3]"></image>
+					<text class="ft16 ftw600 cl-main ml15">热播推荐</text>
+				</view>
+				<navigator url="/pages/client/tuan/ss">
+				<view class="ft14 cl-notice">更多</view>
+				</navigator>
+			</view>
+			<view class="flex space" style="display:flex; flex-wrap:wrap;">
+				<block v-for="(value,key) in datasa" :key="key">
+				<view class="mt10" style="width: 49%; position: relative;">
+					<view class="box"  @click="detail(value.vod_id)">
+						<view class="btntpsdz">{{value.type_name}}</view>
+						<image class="tuan-product-l" mode="aspectFill" :src="value.vod_pic?value.vod_pic:value.vod_pic"></image>
+					</view>
+					<view class="mt8 ft14 ftw500 text-over cl-main" style="text-align: left;">{{value.vod_name}}</view>
+					<view class="mt5 ft12 ftw400 text-over cl-main" style="text-align: left; color: #999;">{{value.vod_blurb}}</view>
+				</view>
+				</block>
+			</view>
+		</view>
+		<view class="home-main">
+			<com-copyright></com-copyright>
+		</view>
+		<dialog-birthday v-if="showBirthday" @closed="showBirthday = false"></dialog-birthday>
+		<dialog-login v-if="showLogin" @loginYes="loginYes" @closed="showLogin = false"></dialog-login>
+		<dialog-qrcode v-if="showQrcode" @closed="showQrcode = false"></dialog-qrcode>
+		<dialog-couponshareget @loginAct="showLoginCouponShareGet" v-if="showCouponShareGet" @closed="showCouponShareGet = false"></dialog-couponshareget>
+	</view>
+</template>
+
+<script>
+	export default{
+		props: ['dataarr','type'],
+		data(){
+			return {
+				datasa:[],
+				isLogin:false,
+				showBirthday:false,
+				showLogin:false,
+				showQrcode:false,
+				showCouponShareGet:false,
+			}
+		},
+
+		created(){
+			this.getrm();
+			// uni.setNavigationBarTitle({
+			//     title: '夜趣小视频'
+			// });
+			
+			// setTimeout(()=>{
+			// 	this.showCouponShareGet = true;	
+			// },1000);
+		},
+		methods:{
+			getrm() {
+				let this_=this
+				let data = {};
+				data.page=1;
+				data.limit=10;
+				data.level=1;//推荐一
+				if(this.type){
+					data.type=this.type;
+				}
+				data.user_random = uni.getStorageSync("userinfo").user_random;
+				uni.request({
+					url: this.configs.Url+'/api.php/User/vods',
+					data: data,
+					success: data => {
+						//console.log(data.data.list);
+						if (data.data.list) {
+							this.datasa=data.data.list
+						}
+					},
+					fail: (data, code) => {
+					}
+				});
+			},
+			//'/pages/client/tuan/detail?id='+value.id
+			detail(id){
+				// #ifndef H5
+				uni.redirectTo({
+					url:'/pages/client/tuan/detail?id='+id
+				})
+				// #endif
+				// #ifdef H5
+				uni.redirectTo({
+					url:'/pages/client/tuan/detail?id='+id
+				})
+				// #endif
+			},
+			showLoginAct(){
+				this.showLogin = true;
+			},
+			showLoginCouponShareGet(){
+				this.showLogin = true;
+			},
+			loginYes(){
+				
+			}
+		}
+	}
+</script>
+
+<style>
+	.home-header{
+		height: 304rpx;
+		width: 100%;
+		position: relative;
+		background: #363B4D;
+		border-radius: 0rpx 0rpx 48rpx 48rpx;
+	}
+	.home-main{
+		width: 100%;
+		position: relative;
+		margin-top: -156rpx;
+		padding: 0 30rpx;
+	}
+	.home-mendian{
+		width: 100%;
+		height: 84rpx;
+		background:rgba(255,255,255,0.1);
+		border-radius: 42rpx;
+	}
+	.tuan-product-l{
+		width: 100%;
+		height: 240rpx;
+		border-radius: 16rpx;
+		background: #F2F2F2;
+	}
+	.tuan-product-r{
+		/* width: calc(100% - 320rpx); */
+	}
+	
+</style>
---
+++ pages/client/components/order/bfjl.vue
@@ -0,0 +1,113 @@
+<template>
+	<view>
+		<view class="box mt16 pd16_15">
+			<view class="order-list-item-tit" style="margin-bottom: 20upx;">
+				<view class="flex alcenter space" >
+					<view class="flex alcenter">
+						<text class="ft14 cl-main">{{datas.out_trade_no}}</text>
+					</view>
+					<text class="ft14 ftw500" :style="{color:getColor}">{{datas.status_means}}</text>
+				</view>
+			</view>
+			<view class="flex">
+				<image mode="aspectFill" class="order-list-item-l" :src="datas.data.pic" style="width: 45%; height: auto; margin-right: 5%; border-radius: 5px;"></image>
+				<view class="order-list-item-r">
+					<view style="width: 300upx;" class="ft14 ftw600 cl-main text-over">{{datas.data.name}}</view>
+					<view class="mt8 ft12 cl-notice">{{datas.ulog_time}}</view>
+					<view class="flex alcenter cl-orange mt8" >
+							<text class="ft12 cl-notice ">消耗积分：{{datas.ulog_points}}</text>
+					</view>
+					<view class="flex alcenter space mt10">
+						<view class="btn-small plan" @click="ulog_del()" style="width: 60px;">删除</view>
+						<view class="btn-small" style="width: 85px;" :style="getBtnStyle">重新观看</view>
+					</view>
+					
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	//status  0,待付款  1待使用    4已过期  8已完成
+	export default{
+		props:{
+			datas:{
+				type:Object,
+				default:function(){
+					return new Object;
+				}
+			}
+		},
+		computed:{
+			getColor(){
+				if(this.datas.status != 0){
+					return  '#000000';
+				}
+				return this.tempColor;
+			}
+		},
+		data(){
+			return {
+				
+			}
+		},
+		created(){
+			
+		},
+		methods:{
+			
+			opuer(sid,smid,splx,lx){
+				console.log(splx)
+				if(splx==1){ //视频
+					uni.navigateTo({
+						url:'/pages/client/tuan/detail?id='+sid+'&mid='+smid
+					})
+				}
+				if(splx==2 && lx==1){
+					uni.navigateTo({
+						url:'/pages/client/tuan/info?id='+sid+'&mid='+smid //音频全集
+					})
+				}
+				if(splx==2 && lx==2){
+					uni.navigateTo({
+						url:'/pages/client/tuan/audio?id='+sid+'&mid='+smid //音频单集
+					})
+				}
+				if(splx==3 && lx==1){
+					uni.navigateTo({
+						url:'/pages/client/tuan/info?id='+sid+'&mid='+smid //文字全集
+					})
+				}
+				if(splx==3 && lx==2){
+					uni.navigateTo({
+						url:'/pages/client/tuan/text?id='+sid+'&mid='+smid //文字单集
+					})
+				}
+				if(splx==4 && lx==1){
+					uni.navigateTo({
+						url:'/pages/client/tuan/infotp?id='+sid+'&mid='+0 //文字全集
+					})
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+	.delivery-type{
+		width: 128rpx;
+		height: 36rpx;
+		background: #FFFFFF;
+		border-radius: 18rpx;
+		font-size: 24rpx;
+		color:#8A8D99;
+		line-height: 36rpx;
+		text-align: center;
+		font-weight: 500;
+		margin-left: 0rpx;
+	}
+	.mt16{
+		margin-top: 0px;
+	}
+</style>
